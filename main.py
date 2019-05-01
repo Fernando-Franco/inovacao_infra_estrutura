db2b17ea18dcfcf47e7b875bd8476428652e6304
negroleo <49699477+negroleo@users.noreply.github.com>
2019-04-30 21:44:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import EmonLib.h
+from import EmonLib.h
 
-Serial.begin(9600)
 EmonLib.EnergyMonitor.current(D0, 111.1) # Corrente: pino de entrada, calibração.
 
 while True:
     Amper = EmonLib.EnergyMonitor.calcIrms(1480)  #Calcular somente Irms
-    Serial.print(Amper*230.0) #Potência aparente 
-    Serial.print(" ")
-    Serial.println(Amper) #Irms
-
-
+    print(Amper*230.0) #Potência aparente 
+    print(Amper) #Irms
